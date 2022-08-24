8d920eafa51139f3c10fb454fc3b32dd9c04f122
Rafael-ID <49356385+ralfId@users.noreply.github.com>
2022-08-24 12:47:13 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import { NavBar } from './Components/NavBar';
+
+function App() {
+  return (
+    <>
+     <NavBar/>
+        
+        <main role="main" className="container">
+        
+        <div className="starter-template">
+            <img src="img/meme.jpg" alt="" srcset=""/>
+            <p className="lead">Éxitos</p>
+        </div>
+        
+        </main>
+    </>
+  );
+}
+
+export default App;
