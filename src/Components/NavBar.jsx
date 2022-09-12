ce2a36d6271995c73065cd336603d0863c910b65
Rafael-ID <49356385+ralfId@users.noreply.github.com>
2022-09-12 15:49:29 -0600

    Update NavBar.jsx

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,20 +4,22 @@ import logo from '../Images/logo.png'
 export const NavBar = () => {
   return (
     <>
-     <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top " >
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top " >
         <a className="navbar-brand" href="!#"> <img srcSet={`${logo}`} alt='logo app' /></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        
-        <div className="collapse navbar-collapse" id="navbarsExampleDefault" style={{marginLeft: '70%'}}>
-           
-            <form className="form-inline my-2 my-lg-0 ">
-            <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
-            <button className="btn btn-secondary my-2 my-sm-0 bg-warning" type="submit"><i class="fas fa-search" style={{color: 'black'}}></i></button>
-            </form>
+
+        <div className="collapse navbar-collapse" id="navbarsExampleDefault" >
+          <ul class="navbar-nav mr-auto">
+
+          </ul>
+          <form className="form-inline my-2 my-lg-0 ">
+            <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-secondary my-2 my-sm-0 bg-warning" type="submit"><i class="fas fa-search" style={{ color: 'black' }}></i></button>
+          </form>
         </div>
-        </nav>
+      </nav>
     </>
   )
 }
